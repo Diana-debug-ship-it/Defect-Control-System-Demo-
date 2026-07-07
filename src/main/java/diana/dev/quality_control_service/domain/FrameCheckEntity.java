6c89b647c49f154c1ef5c3d656bc3e50eb092cbb
Diana Radchuk <dupshur@bk.ru>
2026-07-08 01:08:07 +0300

    feat: added FrameCheckEntity.java

---
+++ src/main/java/diana/dev/quality_control_service/domain/FrameCheckEntity.java
@@ -0,0 +1,44 @@
+package diana.dev.quality_control_service.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "frame_checks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FrameCheckEntity {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "frame_check_seq"
+    )
+    @SequenceGenerator(
+            name = "frame_check_seq",
+            sequenceName = "frame_check_id_seq",
+            allocationSize = 1
+    )
+    private Long id;
+
+    @Column(name = "frame_id", nullable = false)
+    private String frameId;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+
+    @Column(name = "has_defect", nullable = false)
+    private Boolean hasDefect;
+
+    @Column(name = "confidence", nullable = false)
+    private Double confidence;
+
+}
